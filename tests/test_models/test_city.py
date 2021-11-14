f34d3e6bf7469a6ace1158b91dff56c9eb166e86
Biniyam <44437166+benbel376@users.noreply.github.com>
2021-11-14 23:27:02 +0300

    test city basemodel

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel"""
+"""for BaseModel"""
 import os
 import time
 import unittest
@@ -9,10 +9,10 @@ from models.city import City
 
 
 class TestCity(unittest.TestCase):
-    """test BaseModel"""
+    """test"""
 
     def test_init(self):
-        """test blank basemodel init"""
+        """blank basemodel"""
         snapshot = datetime.now()
         cm1 = City()
         snapshot2 = datetime.now()
@@ -36,7 +36,7 @@ class TestCity(unittest.TestCase):
         del cm1
         
     def test_init_dict(self):
-        """test dict basemodel init"""
+        """dictionery of basemodel init"""
         test_dict = {'updated_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')
                      , 'id': 'z3854b62-93fa-fbbe-27de-630706f8313c', 'created_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')}
         cm2 = City(**test_dict)
@@ -54,7 +54,7 @@ class TestCity(unittest.TestCase):
         del cm2
 
     def test_attribute(self):
-        """asdad"""
+        """parent"""
         cm3 = City()
 
         self.assertTrue(hasattr(cm3, "state_id"))
