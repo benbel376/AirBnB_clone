aeec5826703fe03729b4fb96fa3a2e35135a22d0
Biniyam <44437166+benbel376@users.noreply.github.com>
2021-11-14 21:53:10 +0300

    library

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
-from models.engine.file_storage import FileStorage
+from . import base_model
+from . import user
+from .engine.file_storage import FileStorage
+
+all = [base_model.BaseModel, user.User]
 
 storage = FileStorage()
 storage.reload()
